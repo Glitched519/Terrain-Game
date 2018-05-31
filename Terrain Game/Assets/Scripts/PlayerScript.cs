bd0a2c68c31928c5b8c6f712e3ae74dd5fd2ea7f
Glitched519 <parthawhatever@gmail.com>
2018-05-31 15:58:20 -0400

    small changes

---
+++ Terrain Game/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+    
+    public float maxHunger, maxThirst, maxHealth;
+    private float hunger, thirst, health;
+    public float hungerIncrease, thirstIncrease, healthIncrease;
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    void Update()
+    {
+        HungerAndThirst();
+        if(health <= 0)
+        {
+            Die();
+        }
+        print(hunger + ", " + thirst);
+    }
+
+    public void HungerAndThirst()
+    {
+        hunger += 1 * Time.deltaTime;
+        thirst += 1 * Time.deltaTime;
+
+        if(hunger >= maxHunger)
+        {
+            Die();
+        }
+        if(thirst >= maxThirst)
+        {
+            Die();  
+        }
+
+    }
+
+    public void Die()
+    {
+        print("Player has died.");
+    }
+}

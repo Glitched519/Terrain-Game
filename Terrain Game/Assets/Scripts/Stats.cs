bd3669fa56deb8ebb9d4d786c7d6ef04dd268be4
Partha Sarker <parthawhatever@gmail.com>
2018-05-31 23:19:22 -0400

    Added stats and other features

---
+++ Terrain Game/Assets/Scripts/Stats.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Stats : MonoBehaviour {
+
+    public Text coordinates, direction;
+    public Transform player;
+    
+    // Use this for initialization
+    void Update () {
+        coordinates.text = "Location: " + player.position;
+        direction.text = "Rotation: " + player.rotation.y * 180;
+
+    }
+
+}

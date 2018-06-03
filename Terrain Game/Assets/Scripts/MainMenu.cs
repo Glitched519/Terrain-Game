6d98a27a8299f5f735a4342228eca59205d132f4
Partha Sarker <parthawhatever@gmail.com>
2018-06-02 22:49:12 -0400

    Made the Menu better

---
+++ Terrain Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame()
+    {
+        SceneManager.LoadScene("Terrain");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}

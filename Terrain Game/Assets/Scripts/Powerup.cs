5e28c90820a575f533e81191d147d987a25c5c79
Partha Sarker <parthawhatever@gmail.com>
2018-06-09 17:35:55 -0400

    added powerup

---
+++ Terrain Game/Assets/Scripts/Powerup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour {
+
+    public GameObject pickupEffect;
+    public float multiplyer = 1.4f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup(other);
+            Debug.Log("Powerup picked up!");
+        }   
+    }
+
+    void Pickup(Collider player)
+    {
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+        player.transform.localScale *= multiplyer;
+        Destroy(gameObject);
+    }
+}

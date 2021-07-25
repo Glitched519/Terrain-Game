acdf6adf12716defb17c47ef2a41be9743b0c49b
Partha Sarker <parthawhatever@gmail.com>
2021-07-24 20:34:04 -0400

    Fix Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,43 +4,14 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
     
-    public float maxHunger, maxThirst, maxHealth;
-    private float hunger, thirst, health;
-    public float hungerIncrease, thirstIncrease, healthIncrease;
 
     void Start()
     {
-        health = maxHealth;
+
     }
 
     void Update()
     {
-        HungerAndThirst();
-        if(health <= 0)
-        {
-            Die();
-        }
-        print(hunger + ", " + thirst);
-    }
 
-    public void HungerAndThirst()
-    {
-        hunger += 1 * Time.deltaTime;
-        thirst += 1 * Time.deltaTime;
-
-        if(hunger >= maxHunger)
-        {
-            Die();
-        }
-        if(thirst >= maxThirst)
-        {
-            Die();  
-        }
-
-    }
-
-    public void Die()
-    {
-        print("Player has died.");
     }
 }
